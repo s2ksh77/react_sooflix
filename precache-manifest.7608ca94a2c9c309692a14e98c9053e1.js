b1a47fe20e4e651d6c4b10c29206abe2332a6b59
soohyun <soohyun_kim2@tmax.co.kr>
2020-06-26 15:52:19 +0900

    Updates

---
+++ precache-manifest.7608ca94a2c9c309692a14e98c9053e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d13beea2cc997c3e51cbeddc55e915",
+    "url": "/sooflix/index.html"
+  },
+  {
+    "revision": "f6e48ec45e8e5ac60ead",
+    "url": "/sooflix/static/js/2.ea3829e2.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/sooflix/static/js/2.ea3829e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4b13528bf0d1273dda",
+    "url": "/sooflix/static/js/main.65e485cb.chunk.js"
+  },
+  {
+    "revision": "0437128045e38caf9406",
+    "url": "/sooflix/static/js/runtime-main.d6256ebc.js"
+  },
+  {
+    "revision": "553349339182a7ab63295d100e916ec0",
+    "url": "/sooflix/static/media/youtube-brands.55334933.png"
+  }
+]);
